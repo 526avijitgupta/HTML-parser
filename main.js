3ecf74891d8c9b4e82fa24f564f890ff11954dab
Avijit Gupta <526avijit@gmail.com>
2015-10-21 00:24:46 +0530

    Used Object-Orientation for stack, code refactoring

--- main.js
+++ main.js
@@ -18,82 +18,80 @@ function makeObj(str) {
     return obj;
 }
 
-var stack = [];
-stack.top = function() {
-    return stack[this.length - 1];
+var Stack = function() {
+    this.data = [];
+    this.size = 0;
 };
 
-stack.pop = function() {
-    var returnVal = stack.top();
-    stack.splice(stack.length-1, 1);
-    return returnVal;
+Stack.prototype = {
+    constructor: Stack,
+    top: function() {
+        return this.data[this.size-1];
+    },
+    push: function(val) {
+        return this.data.push(val) && this.size++;
+    },
+    pop: function() {
+        return this.data.pop() && this.size--;
+    },
+    print: function() {
+        for (var i = 0; i < this.size; i++) {
+            console.log(this.data[i]);
+        }
+    },
+    isEmpty: function() {
+        return this.size === 0 ? true : false;
+    }
 };
-
-function printStack() {
-    stack.map(function(elm) {console.log(elm)});
-}
 
 // Static HTML for now..
-var HTML = "<html><head></head><body><div>This is a div</div></body></html>";
-var prevTagStartIndex = 0,
-    prevTagEndIndex = 0,
-    currTagStartIndex = 0,
-    currTagEndIndex = 0;
+// var HTML = "<html><head></head><body><div><span></span></div></body></html>";
+// var HTML = "<html><head></head><body><div>This is a div</div></body></html>";
+var HTML = "<html><head></head><body><div>This is a div</div></html>";
 
-var tempStripStart = 1;
+var symStack = new Stack();
+var indexStack = new Stack();
 
-// while (HTML) {
-for (var i = 0; i < 100; i++) {
-
-    if (!HTML) {
-        break;
-    }
-    
-    // Tag start
-    else if (HTML.indexOf("</") === 0) {
-        if (!assertStartBeforeEnd("tag")) continue;
-        stack.pop();
-        currTagEndIndex = HTML.indexOf("</", prevTagEndIndex);
-        var elm = HTML.match(TAG_END_REGEX);
-        if (elm) {
-            elem = elm[0];
+if (typeof HTML !== 'undefined') {
+    while (HTML.length >= 1) {
+        if (!HTML) {
+            break;
         }
-        console.log('Close: ', elem);
-        HTML = HTML.split(elem)[1];
-    }
-    // Tag End
-    else if (HTML.indexOf("<") === 0) {
-        stack.push("<");
-        currTagStartIndex = HTML.indexOf("<", prevTagStartIndex);
-        var elm = HTML.match(TAG_START_REGEX);
-        if (elm) {
-            elem = elm[0];
+        // Tag start
+        else if (HTML.indexOf("</") === 0) {
+            if (!assertStartBeforeEnd("tag")) break;
+            symStack.pop();
+            var elm = HTML.match(TAG_END_REGEX);
+            console.log('Close: ', elm[2].substr(1));
+            HTML = HTML.split(elm[0])[1];
         }
-        console.log('Open: ', elem);
-        HTML = HTML.split(elem)[1];
+        // Tag End
+        else if (HTML.indexOf("<") === 0) {
+            symStack.push("<");
+            var elm = HTML.match(TAG_START_REGEX);
+            console.log('Open: ', elm[1]);
+            HTML = HTML.split(elm[0])[1];
+        }
+        // Comment Start
+        else if (HTML.indexOf("<!--") === 0) {
+        }
+        // Comment End
+        else if (HTML.indexOf("-->") === 0) {
+            if (!assertStartBeforeEnd("comment")) continue;
+        }
+        else {
+            HTML = HTML.substr(1);
+        }
     }
-    
-    // Comment Start
-    else if (HTML.indexOf("<!--") === 0) {
-    }
-    // Comment End
-    else if (HTML.indexOf("-->") === 0) {
-        if (!assertStartBeforeEnd("comment")) continue;
-    }
-    else {
-        // validateHTML();
-        HTML = HTML.substr(tempStripStart);
+    if (!symStack.isEmpty()) {
+        console.log('Some error');
     }
 }
 
 function assertStartBeforeEnd(type) {
-    if (type !== "tag" && type !== "comment") {
-        return;
-    }
-    if (type === "tag") {
-        return stack.top() === "<" ? true : false;
-    }
-    else {
-        return stack.top() === "<!--" ? true : false;
-    }
+    return type === "tag" ?
+        (symStack.top() === "<" ? true : false) :
+    type === "comment" ?
+        (symStack.top() === "<!--" ? true : false) :
+    "";
 };
